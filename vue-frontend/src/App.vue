039c6eaf3cab16620b54f4c31b046ae6ad93d8d0
Haejin Jo <haejin@hackerone.com>
2022-07-19 17:36:54 -0400

    Add vue scaffolding that prints query for first page of posts on hello world page

---
+++ vue-frontend/src/App.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+import { onMounted, ref } from 'vue'
+
+const posts = ref([]);
+
+onMounted(() => {
+  fetch('http://localhost/graphql', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: `
+        query {
+          posts(page:1){
+            data {
+              created_at
+              id
+              title
+              body
+            }
+          }
+        }
+      `
+    }),
+  })
+    .then(res => res.json())
+    .then(result => {
+      console.log(result);
+      posts.value = result.data.posts.data
+    });
+});
+</script>
+
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+  <ul>
+    <li v-for="post in posts" :key="post.id">{{ post.title }}</li>
+  </ul>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
